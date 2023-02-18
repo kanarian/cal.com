93c8a4ad667f09ac454bab23b989fefa5d947bbd
kanarian <arian.joyandeh@hotmail.com>
2023-02-18 13:41:55 +0100

    +10 to 0

--- packages/lib/slots.ts
+++ packages/lib/slots.ts
@@ -81,7 +81,7 @@ function buildSlots({
     for (let slotStart = boundaryStart; slotStart < boundaryEnd; slotStart += frequency) {
       computedLocalAvailability.forEach((item) => {
         // TODO: This logic does not allow for past-midnight bookings.
-        if (slotStart < item.startTime || slotStart > item.endTime + 10 - eventLength) {
+        if (slotStart < item.startTime || slotStart > item.endTime - eventLength) {
           return;
         }
         slotsTimeFrameAvailable[slotStart.toString()] = {
